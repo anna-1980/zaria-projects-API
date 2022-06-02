8addcffd0dc37b4eab8caedab7108cd2527b1c28
Anna Zdyb <anna.zdyb@gmail.com>
2022-06-02 13:34:28 +0200

    upload image with multer and add endpoint to retrieve that image

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,1 +1,10 @@
 // create a mini API (microservice architecture) taht only does ONE thing
+import ErrorResponse from "../utils/errorResponse.js";
+
+export const uploadResponse = (req, res) =>{   //only allow upload picture (validation in imageUploader.js)
+    const {file} = req;
+    if (!file) throw new  ErrorResponse('Please include a file to upload'); // 400 Bad Request
+     
+    
+    res.send({location: `/${file.filename}`});
+};
--- routes/uploadRouter.js
+++ routes/uploadRouter.js
@@ -1,17 +1,10 @@
 import { Router } from 'express';
 import verifyToken from '../middlewares/verifyToken.js';
 import imageUploader from '../middlewares/imageUploader.js';
- 
+import {uploadResponse}   from '../controllers/uploadController.js';
 
 const uploadRouter = Router();
 
-uploadRouter.post('/', verifyToken, imageUploader.single('profile_pic'), (req, res) =>{   //only allow upload picture (validation in imageUploader.js)
-    const {file} = req
-    if (!file) {
-      return res.status(400).json({error :'Please include a file to upload'}); // 400 Bad Request
-    }
-    
-    res.send({location: `/${file.filename}`});
-});    
+uploadRouter.post('/', verifyToken, imageUploader.single('profile_pic'), uploadResponse);    
 
 export default uploadRouter;
